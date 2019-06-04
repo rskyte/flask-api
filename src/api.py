53eece36a32e8b1efc6efa3326dded428c7d905d
Rhys Kyte <rskyte@gmail.com>
2019-06-04 20:55:04 +0100

    user data can be posted to api

--- src/api.py
+++ src/api.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
-    def hello():
+    def index():
         return "<h1>Flask API<h1>"
+
+    @app.route("/api/users", methods=["POST"])
+    def create_user():
+        user_id = request.json.get('userId')
+        name = request.json.get('name')
+        return "User: %s, created." % user_id
     
     return app
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -17,10 +17,16 @@ def client(app):
     client = app.test_client()
     return client
 
-def test_app_route_returns_ok_status_code(client):
+def test_api_route_returns_ok_status_code(client):
     response = client.get("/")
     assert response.status_code == 200
 
-def test_app_route_has_welcome_page(client):
+def test_api_route_has_welcome_page(client):
     response = client.get("/")
     assert response.data == "<h1>Flask API<h1>"
+
+def test_api_can_receive_user_data(client):
+    test_data = {'userId': 'jbloggs', 'name': 'Joseph Bloggs'}
+    response = client.post("/api/users", json=test_data)
+    assert response.status_code == 200
+    assert response.data == "User: jbloggs, created."
