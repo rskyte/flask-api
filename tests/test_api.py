dfd34ee1fd1cd2579b25fcd7b2823e80ca8fc819
Rhys Kyte <rskyte@gmail.com>
2019-06-04 21:26:15 +0100

    test edit for pipelines

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,9 +21,10 @@ def test_api_route_returns_ok_status_code(client):
     response = client.get("/")
     assert response.status_code == 200
 
-def test_api_route_has_welcome_page(client):
+def test_api_route_has_welcome_page(client):   
     response = client.get("/")
-    assert response.data == "<h1>Flask API<h1>"
+    value = response.data
+    assert value == "<h1>Flask API<h1>"
 
 def test_api_can_receive_user_data(client):
     test_data = {'userId': 'jbloggs', 'name': 'Joseph Bloggs'}
